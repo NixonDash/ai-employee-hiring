758c0c9d52e4aae1e77191ee2f4425553d2dfc4d
Djordje <82043284+NixonDash@users.noreply.github.com>
2023-03-19 12:34:25 +0100

    Update pricing.js

--- pages/pricing.js
+++ pages/pricing.js
@@ -1,1 +1,18 @@
+// pages/pricing.js
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
+const Pricing = () => {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h1>Pricing</h1>
+        <p>Choose an industry to view pricing for our AI employees.</p>
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Pricing;
