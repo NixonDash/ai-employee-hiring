42c4951d79af58040b6a3b0925a481e7c1e7daa3
Djordje <82043284+NixonDash@users.noreply.github.com>
2023-03-19 12:47:51 +0100

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -1,14 +1,77 @@
 // pages/contact.js
+import { useState } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('/api/contact', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        alert('Your message has been sent. Thank you!');
+        setFormData({ name: '', email: '', message: '' });
+      } else {
+        alert('There was an error sending your message.');
+      }
+    } catch (error) {
+      console.error(error);
+      alert('There was an error sending your message.');
+    }
+  };
+
   return (
     <div>
       <Header />
       <main>
         <h1>Contact Us</h1>
-        <p>Get in touch with our team.</p>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+
+          <label htmlFor="message">Message:</label>
+          <textarea
+            id="message"
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+            required
+          />
+
+          <button type="submit">Send</button>
+        </form>
       </main>
       <Footer />
     </div>
