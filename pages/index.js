eb4de65de078732974b9b69c10aa6cf901fa0a1c
Djordje <82043284+NixonDash@users.noreply.github.com>
2023-03-19 12:32:46 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,16 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h1>Welcome to AI Employee Hiring</h1>
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
