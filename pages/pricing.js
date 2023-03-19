3f20ec583a75705a43aee0fbc8c94e9f0fd6c047
Djordje <82043284+NixonDash@users.noreply.github.com>
2023-03-19 12:45:06 +0100

    Update pricing.js

--- pages/pricing.js
+++ pages/pricing.js
@@ -1,14 +1,34 @@
 // pages/pricing.js
+import { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 const Pricing = () => {
+  const [pricingData, setPricingData] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/pricing')
+      .then((response) => response.json())
+      .then((data) => setPricingData(data));
+  }, []);
+
   return (
     <div>
       <Header />
       <main>
         <h1>Pricing</h1>
-        <p>Choose an industry to view pricing for our AI employees.</p>
+        {pricingData.map((industryData) => (
+          <div key={industryData.id}>
+            <h2>{industryData.industry}</h2>
+            <ul>
+              {industryData.pricing.map((priceData) => (
+                <li key={priceData.role}>
+                  {priceData.role}: ${priceData.price} / month
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
       </main>
       <Footer />
     </div>
