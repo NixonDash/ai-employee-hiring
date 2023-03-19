6562adf5497461529590e8c7a6f7ea52b6bd669e
Djordje <82043284+NixonDash@users.noreply.github.com>
2023-03-19 12:31:17 +0100

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,1 +1,28 @@
+// components/Header.js
+import Link from 'next/link';
 
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+        <Link href="/about">
+          <a>About Us</a>
+        </Link>
+        <Link href="/ai-employees">
+          <a>AI Employees</a>
+        </Link>
+        <Link href="/pricing">
+          <a>Pricing</a>
+        </Link>
+        <Link href="/contact">
+          <a>Contact Us</a>
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
