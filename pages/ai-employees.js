2f33d8e6f1c65789236c6e67e38d1fb39574e9b7
Djordje <82043284+NixonDash@users.noreply.github.com>
2023-03-19 12:42:05 +0100

    Update ai-employees.js

--- pages/ai-employees.js
+++ pages/ai-employees.js
@@ -1,14 +1,29 @@
 // pages/ai-employees.js
+import { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 const AIEmployees = () => {
+  const [aiEmployees, setAIEmployees] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/ai-employees')
+      .then((response) => response.json())
+      .then((data) => setAIEmployees(data));
+  }, []);
+
   return (
     <div>
       <Header />
       <main>
         <h1>AI Employees</h1>
-        <p>Explore our AI employees by industry.</p>
+        <ul>
+          {aiEmployees.map((employee) => (
+            <li key={employee.id}>
+              {employee.name} - {employee.industry}
+            </li>
+          ))}
+        </ul>
       </main>
       <Footer />
     </div>
