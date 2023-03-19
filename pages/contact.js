92b30957371e4ef8bd3eda3ce86a776910d1b894
Djordje <82043284+NixonDash@users.noreply.github.com>
2023-03-19 12:34:50 +0100

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -1,1 +1,18 @@
+// pages/contact.js
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
+const Contact = () => {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h1>Contact Us</h1>
+        <p>Get in touch with our team.</p>
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Contact;
