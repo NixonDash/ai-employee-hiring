1d377dd7284c2efdf9f34998442a987af68d4211
Djordje <82043284+NixonDash@users.noreply.github.com>
2023-03-19 12:34:09 +0100

    Update ai-employees.js

--- pages/ai-employees.js
+++ pages/ai-employees.js
@@ -1,1 +1,18 @@
+// pages/ai-employees.js
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
+const AIEmployees = () => {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h1>AI Employees</h1>
+        <p>Explore our AI employees by industry.</p>
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default AIEmployees;
