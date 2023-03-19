79026ea12877e9d2902ccafa5e8790fe22402831
Djordje <82043284+NixonDash@users.noreply.github.com>
2023-03-19 12:31:31 +0100

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,1 +1,11 @@
+// components/Footer.js
 
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; {(new Date().getFullYear())} AI Employee Hiring. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
