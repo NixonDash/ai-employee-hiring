880045a63dd629a8908caed956bf2cca4bcfe52f
Djordje <82043284+NixonDash@users.noreply.github.com>
2023-03-19 12:33:54 +0100

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,1 +1,18 @@
+// pages/about.js
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
+const About = () => {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h1>About Us</h1>
+        <p>Information about our company and mission.</p>
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default About;
